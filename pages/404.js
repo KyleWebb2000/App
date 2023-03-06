1a6b11cfb3836311999500d96cd3ed484bd606ae
Kyle Webb <Kylewebb3577@gmail.com>
2023-03-06 20:57:18 +0000

    feat: 404 page now redirects user

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,25 @@
-export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-  }
+import styles from '../components/layout.module.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from 'next/link';
+
+const notFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 8000)
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <h1>Ooops...</h1>
+      <h2>It looks like you're lost...</h2>
+      <p>Wait while we redirect you</p>
+      <p>Or click <Link href={`/`}>here</Link> to be redirected to the home page.</p>
+    </div>
+  );
+}
+
+export default notFound;
