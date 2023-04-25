e1e8e4950e3af50669f646eecc5a24765c438f05
Kyle Webb <Kylewebb3577@gmail.com>
2023-04-25 21:46:53 +0100

    fix: My skills mobile layout fixed and new home button added

--- pages/posts/contact-me.js
+++ pages/posts/contact-me.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import styles from '../../styles/contact-me.module.css';
 
 export default function ContactMe() {
   return (
@@ -8,10 +11,17 @@ export default function ContactMe() {
       <Head>
         <title>Contact Me</title>
       </Head>
-      <h1>Contact Me</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <div className={styles.header}>
+        <div><h1>Contact Me</h1></div>
+        <div>
+        <h2>
+          <Link href="/"><FontAwesomeIcon icon={faHome}></FontAwesomeIcon></Link>
+        </h2>
+        </div>
+      </div>
+      <div className={styles.contactInfo}>
+        <p>Email: Kylewebb3577@gmail.com</p>
+      </div>
     </Layout>
   )
 }
